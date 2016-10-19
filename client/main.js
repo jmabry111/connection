8309ab218a85246b761c7f0efe8a3cc51ea62aa5
Jason Mabry <jmabry@mymabry.com>
2016-10-19 13:29:10 -0400

    add flash message

--- client/main.js
+++ client/main.js
@@ -1,3 +1,10 @@
 Template.registerHelper('formatDate', function(createdAt) {
     return createdAt.toLocaleDateString();
 });
+
+AutoForm.addHooks(['insertCardForm'], {
+  onSuccess: function(operation, result, template) {
+    FlashMessages.sendSuccess('Thank you for connecting with SMCC!');
+    FlowRouter.go("/");
+  }
+});
