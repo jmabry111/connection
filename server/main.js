e3a65e2d8ec4fb0143badd3cf91503745a22d2cc
Jason Mabry <jmabry@mymabry.com>
2016-11-15 12:23:44 -0500

    show view cards link if user has admin role

--- server/main.js
+++ server/main.js
@@ -2,4 +2,21 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if(Meteor.users.find().count() < 1) {
+    var users = [
+      {email: 'jason@mymabry.com', roles: ['admin']}
+    ];
+
+    _.each(users, function(user) {
+      var id = Accounts.createUser({
+        email: user.email,
+        password: "password"
+      });
+      Meteor.users.update(
+        {_id: id},
+        { $set: {'emails.0.verified' : false}}
+      );
+      Roles.addUsersToRoles(id, user.roles);
+    });
+  }
 });
