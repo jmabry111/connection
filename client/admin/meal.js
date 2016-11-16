8b1997e90ac99250f436b9bc955cd3a2b40578f2
Jason Mabry <jmabry@mymabry.com>
2016-11-16 11:30:53 -0500

    edit meal info

--- client/admin/meal.js
+++ client/admin/meal.js
@@ -3,5 +3,8 @@ Meteor.subscribe('weekinfo');
 Template.meal.helpers({
   meal: ()=>{
     return WeekInfo.findOne();
+  },
+  updateWeekInfoId: function() {
+    return WeekInfo.findOne()._id;
   }
 });
--- collections/weekinfo.js
+++ collections/weekinfo.js
@@ -3,11 +3,13 @@ WeekInfo = new Mongo.Collection('weekinfo');
 WeekInfoSchema = new SimpleSchema({
   isMeal: {
     type: Boolean,
-    label: "Is there a meal this week?"
+    label: "Is there a meal this week?",
+    optional: true
   },
   currentMeal: {
     type: String,
-    label: "This week's meal"
+    label: "This week's meal",
+    optional: true
   }
 });
 
